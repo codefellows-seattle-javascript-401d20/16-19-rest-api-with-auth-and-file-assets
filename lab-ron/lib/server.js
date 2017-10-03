8189e73d6cbfbacd6694b3c50406a587a232bc71
Ron Barrantes <ronbarrantes@gmail.com>
2017-10-03 16:14:00 -0700

    account model done

--- lab-ron/lib/server.js
+++ lab-ron/lib/server.js
@@ -15,4 +15,34 @@ app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(morgan(production ? 'combined' : 'dev'));
 
 // routes
+app.use(require('../route/auth-route.js'));
+app.use(require('../route/profile-route.js'));
+app.all('*', (req, res) => res.sendStatus(400));
+app.use(require('./error-middleware.js'));
 
+module.exports = {
+  start: () => {
+    return new Promise((resolve, reject) => {
+      if (server)
+        return reject(new Error('SERVER ERROR: server is running'));
+      server = app.listen(process.env.PORT, () => {
+        console.log('SERVER ON:', process.env.PORT);
+        return resolve();
+      });
+
+    })
+      .then(() => mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true }));
+  },
+  stop: () => {
+    return new Promise((resolve, reject) => {
+      if (!server)
+        return reject(new Error('SERVER ERROR: server is off'));
+      server.close(() => {
+        server = null;
+        console.log('SERVER OFF');
+        return resolve();
+      });
+    })
+      .then(() => mongoose.disconnect());
+  },
+};
