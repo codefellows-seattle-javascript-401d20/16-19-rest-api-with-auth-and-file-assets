1cc35a145a549ca2362a134cbfc9be8951ab868f
Ron Barrantes <ronbarrantes@gmail.com>
2017-10-04 23:09:19 -0700

    working on the aws image uploader

---
+++ lab-ron/model/image.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const imageSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  description: { type: String, required: false },
+  alt: { type: String, required: false },
+  account: { type: mongoose.Schema.Types.ObjectId, required: true },
+  created: { type: Date, default: () => { } },
+});
+
+module.exports = mongoose.model('image', imageSchema);
