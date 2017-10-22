a1fa9b63ceed929095b24172f8f62f7712c56dfe
Christina Thomas <Penssake@gmail.com>
2017-10-22 10:06:21 -0700

    start test

---
+++ lab-christina/__test__/auth-router.test.js
@@ -0,0 +1,48 @@
+require('./lib/setup.js');
+const superagent = require('superagent');
+const server = require('../server.js');
+const accountMock = require('.lib/account-mock.js');
+
+const apiURL = `hhtp://localhost:${process.env.PORT}`;
+
+console.log('apiURL' apiURL);
+describe('AUTH router', () => {
+  beforeAll(server.start)
+  afterAll(server.stop)
+  afterEach(accountMock.remove)
+
+  test('POST /signup with 200', () => {
+    return superagent.post(`${apiURL}/signup`)
+    .send({
+      userName: 'penssake',
+      email: 'penssake@test.com',
+      password: 'secret',
+    })
+    .then(response => {
+      expect(response.status).toEqual(200)
+      expect(response.body.token).toBeTruthy()
+    })
+  })
+
+  test('POST /signup with 400', () => {
+    return superagent.post(`${apiURL}/signup`)
+    .send({
+      email: 'penssake@test.com',
+      password: 'secret',
+    })
+    .then(Promise.reject)
+    .catch(response => {
+      expect(response.status(400))
+    })
+  })
+
+  test('', () => {
+  })
+
+  test('', () => {
+  })
+
+  test('', () => {
+  })
+  
+})
